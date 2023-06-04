372ea4f95324152d700b0f74a405c6ab635b1c35
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2023-06-04 15:31:09 +0200

    get / & get/:contactId

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,28 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const readContacts = async () => {
+  return await fs
+    .readFile(contactsPath, "utf-8")
+    .then((data) => JSON.parse(data));
+};
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  return await readContacts();
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await readContacts();
+  return contacts.find((el) => el.id === contactId);
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {};
+
+const addContact = async (body) => {};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +30,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
